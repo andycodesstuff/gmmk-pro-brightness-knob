d121e8c656014ab27183fef9bb20241ab35938e8
andycodesstuff <andrybusa@gmail.com>
2023-06-10 18:14:25 +0100

    Add mouse delta calculation

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,31 @@ use windows::Win32::Foundation::{HMODULE, HWND, LPARAM, LRESULT, WPARAM};
 use windows::Win32::System::Threading::GetCurrentThreadId;
 use windows::Win32::UI::WindowsAndMessaging::{
   CallNextHookEx, DispatchMessageW, GetMessageW, PostThreadMessageW, SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx,
-  HHOOK, MSG, WINDOWS_HOOK_ID, WM_QUIT
+  HHOOK, MSG, MSLLHOOKSTRUCT, WINDOWS_HOOK_ID, WM_QUIT
 };
 
+const HC_ACTION: i32 = 0;
 const WH_MOUSE_LL: WINDOWS_HOOK_ID = WINDOWS_HOOK_ID(14);
+const WM_MOUSEWHEEL: WPARAM = WPARAM(522usize);
 
+/// Handle low-level mouse input events
+/// 
+/// Note: When a WH_MOUSE_LL hook is registered, all the data is stored in a MSLLHOOKSTRUCT struct pointed by the LPARAM
+///       argument
+/// Reference: https://stackoverflow.com/a/68827449
 unsafe extern "system" fn mouse_hook(code: i32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-  println!("mouse event");
+  if code == HC_ACTION && w_param == WM_MOUSEWHEEL {
+    // Dereference the pointer to get the mouse input event data, then extract the high-order word (the first 2 bytes)
+    // of the mouseData member to get the mouse delta. After casting it to a short int, a positive value indicates that
+    // the wheel was rotated forward, away from the user; a negative value indicates that the wheel was rotated
+    // backward, towards the user
+    // 
+    // Reference: https://learn.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-msllhookstruct#members
+    let mouse_event = *(l_param.0 as *const MSLLHOOKSTRUCT);
+    let mouse_delta = ((mouse_event.mouseData >> 16) & 0xffff) as u16 as i16;
+
+    println!("mouse delta: {:?} {}", mouse_event, mouse_delta);
+  }
 
   CallNextHookEx(HHOOK(0), code, w_param, l_param)
 }
